e50051daf5c3f58ddfc8df2071e9fe58f62cfa38
Lennart Hengstmengel <github@vazaha.nl>
2023-04-05 11:46:30 +0200

    formatting

--- app/Collections/TimeEntryCollection.php
+++ app/Collections/TimeEntryCollection.php
@@ -12,7 +12,10 @@ class TimeEntryCollection extends Collection
     /** @return \Illuminate\Support\Collection|array<array-key, \Carbon\Carbon>  */
     public function getUniqueDates(): Collection
     {
-        return $this->groupBy(fn (TimeEntry $timeEntry) => $timeEntry->started_at->format('Y-m-d'))
+        return $this
+            ->groupBy(
+                fn (TimeEntry $timeEntry) => $timeEntry->started_at->format('Y-m-d')
+            )
             ->keys()
             ->unique()
             ->sort()
@@ -37,14 +40,15 @@ class TimeEntryCollection extends Collection
 
     public function getDateString(): string
     {
-        return $this->getUniqueDates()
+        return $this
+            ->getUniqueDates()
             ->chunkWhile(function (Carbon $date, $key, $chunk) {
                 return $date->diffInDays($chunk->last(), true) === 1;
             })
             ->map(function (Collection $dates) {
-                /** @var Carbon $firstDate */
+                /** @var \Carbon\Carbon $firstDate */
                 $firstDate = $dates->first();
-                /** @var Carbon $lastDate */
+                /** @var \Carbon\Carbon $lastDate */
                 $lastDate = $dates->last();
 
                 if ($dates->count() === 1) {
@@ -52,17 +56,28 @@ class TimeEntryCollection extends Collection
                 }
 
                 if ($firstDate->month === $lastDate->month) {
-                    return sprintf('%s-%s/%s', $firstDate->format('d'), $lastDate->format('d'), $firstDate->format('m'));
+                    return sprintf(
+                        '%s-%s/%s',
+                        $firstDate->format('d'),
+                        $lastDate->format('d'),
+                        $firstDate->format('m')
+                    );
                 }
 
-                return $firstDate->format('d/m') . ' - ' . $lastDate->format('d/m');
+                return $firstDate->format('d/m') .
+                    ' - ' .
+                    $lastDate->format('d/m');
             })
             ->join(', ');
     }
 
     public function getDescription(): string
     {
-        return sprintf('%s (%s)', $this->first()->project->name, $this->getDateString());
+        return sprintf(
+            '%s (%s)',
+            $this->first()->project->name,
+            $this->getDateString()
+        );
     }
 
     public function getRate(): ?float
@@ -70,7 +85,7 @@ class TimeEntryCollection extends Collection
         /** @var \App\Models\TimeEntry $timeEntry */
         $timeEntry = $this->first();
 
-        // TODO get/return default workspace rate as fallback
+        // TODO get/return default rate as fallback
         return $timeEntry->project->rate;
     }
 }
